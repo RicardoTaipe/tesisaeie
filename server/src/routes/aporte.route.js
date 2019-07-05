264750c6c134e3f0467a24272b9229eb53ab0a90
RicardoTaipe <rdavid28@hotmail.es>
2019-07-04 21:16:26 -0500

    all modules finished

--- server/src/routes/aporte.route.js
+++ server/src/routes/aporte.route.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require("../middleware/auth");
 const aporteController = require("../controllers/aporte.controller");
 
 //Show all aportes
 router.get("/", aporteController.get_all_aportes);
 //Add a new aporte
-router.post("/", aporteController.create_aporte);
+router.post("/", auth, aporteController.create_aporte);
 //Show a aporte by Id
-router.get("/:aporteId", aporteController.get_aporte);
+router.get("/:aporteId", auth, aporteController.get_aporte);
 //Update a aporte by Id
-router.put("/:aporteId", aporteController.update_aporte);
+router.put("/:aporteId", auth, aporteController.update_aporte);
 //Delete a aporte by Id
-router.delete("/:aporteId", aporteController.delete_aporte);
+router.delete("/:aporteId", auth, aporteController.delete_aporte);
 
 module.exports = router;
